f09e5ff5cc878d3182c2405169c5afbf2c5748a4
Or Mizrahi <or0mizrahi@gmail.com>
2023-01-30 07:59:47 +0200

    Adding a new transaction, structure changes

---
+++ lib/widgets/new_transaction.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NewTransaction extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+  final Function addTransaction;
+
+  NewTransaction(this.addTransaction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              // onChanged: (value) => titleInput = value,
+              controller: titleController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              // onChanged: (value) => amountInput = value,
+              controller: amountController,
+            ),
+            TextButton(
+              child: Text('Add Transaction'),
+              onPressed: (() {
+                addTransaction(titleController.text, double.parse(amountController.text));
+              }),
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.indigoAccent),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
